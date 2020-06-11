047039f3880774da58090704c92fc35732f14d7d
choobani <saeed.choobani@gmail.com>
2020-06-11 16:00:01 +0200

    updated regarding heloku

--- main.py
+++ main.py
@@ -14,6 +14,6 @@ def create_app():
     return app
 
 
-# if __name__ == '__main__':
-app = create_app()
-app.run(debug=True, use_reloader=False)
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True, use_reloader=False)
